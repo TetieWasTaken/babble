67da6c3a151a03a7b1b6f30dcea15fdee3bd47ff
TetieWasTaken <tjvssr@gmail.com>
2025-05-17 12:10:18 +0200

    feat(core): tsdoc

--- src/core/index.ts
+++ src/core/index.ts
@@ -10,6 +10,10 @@ import logger from "../linker/logger.js";
 
 const dataFile = path.resolve(process.cwd(), "data", "db.json");
 
+/**
+ * Reads the entire database
+ * @returns the existing data
+ */
 async function _read(): Promise<Record<string, any>> {
 	try {
 		const raw = await fs.readFile(dataFile, "utf-8");
@@ -20,6 +24,10 @@ async function _read(): Promise<Record<string, any>> {
 	}
 }
 
+/**
+ * Overwrites the database
+ * @param data the data to write
+ */
 async function _write(data: Record<string, any>): Promise<void> {
 	try {
 		const json = JSON.stringify(data, null, 2);
@@ -29,21 +37,41 @@ async function _write(data: Record<string, any>): Promise<void> {
 	}
 }
 
+/**
+ * Get any value in the given object
+ * @param obj the object to read from
+ * @param pathParts the path to read
+ * @returns the value stored at that path
+ */
 function _get(obj: any, pathParts: string[]): any {
 	return pathParts.reduce((acc, key) => (acc != null && typeof acc === "object" ? acc[key] : undefined), obj);
 }
 
+/**
+ * Sets any value in the given object
+ * @param obj the object to set the value in
+ * @param pathParts the path to write to
+ * @param value the value to write
+ */
 function _set(obj: any, pathParts: string[], value: any): void {
 	const lastKey = pathParts.pop()!;
 	const parent = pathParts.reduce((acc, key) => {
 		if (acc[key] == null || typeof acc[key] !== "object") {
 			acc[key] = {};
 		}
+
 		return acc[key];
 	}, obj);
+
 	parent[lastKey] = value;
 }
 
+/**
+ * Deletes any kv pair in the given object
+ * @param obj the object to delete the kv from
+ * @param pathParts the path to delete
+ * @returns if the operation was successful
+ */
 function _delete(obj: any, pathParts: string[]): boolean {
 	const lastKey = pathParts.pop()!;
 	const parent = pathParts.reduce((acc, key) => (acc != null && typeof acc === "object" ? acc[key] : undefined), obj);
@@ -51,9 +79,20 @@ function _delete(obj: any, pathParts: string[]): boolean {
 		delete parent[lastKey];
 		return true;
 	}
+
 	return false;
 }
 
+/**
+ * Adds a value to the database
+ * @param path the slash-delimited key path to write to
+ * @param doc the value to write
+ *
+ * @remarks
+ * internally similar to {@link modify}
+ *
+ * @returns the updated database
+ */
 export async function add(path: string, doc: any) {
 	const db = await _read();
 	const parts = path.split("/").filter(Boolean);
@@ -62,12 +101,23 @@ export async function add(path: string, doc: any) {
 	return db;
 }
 
+/**
+ * Fetches a value from the database
+ * @param path the slash-delimited key path to read
+ * @returns the fetched value
+ */
 export async function fetch(path: string) {
 	const db = await _read();
 	const parts = path.split("/").filter(Boolean);
 	return _get(db, parts);
 }
 
+/**
+ * Modifies a value in the database
+ * @param path the slash-delimited key path to modify
+ * @param patch the new value
+ * @returns the updated database
+ */
 export async function modify(path: string, patch: any) {
 	const db = await _read();
 	const parts = path.split("/").filter(Boolean);
@@ -84,6 +134,11 @@ export async function modify(path: string, patch: any) {
 	return db;
 }
 
+/**
+ * Removes a kv pair from the database
+ * @param path the slash-delimited key path to remove
+ * @returns the updated database
+ */
 export async function remove(path: string) {
 	const db = await _read();
 	const parts = path.split("/").filter(Boolean);
