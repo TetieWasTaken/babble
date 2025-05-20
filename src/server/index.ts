b17a3f197d3a79072e993a4e5947c22ff18beb51
TetieWasTaken <tjvssr@gmail.com>
2025-05-20 20:40:56 +0200

    feat: require authentication

--- src/server/index.ts
+++ src/server/index.ts
@@ -8,11 +8,29 @@
  */
 
 import fastifyFactory from 'fastify';
+import basicAuth from '@fastify/basic-auth';
 import logger from '../linker/logger.js';
 import routes from './routes.js';
 
+const users = {
+	alice: 'password123',
+	bob: 'hunter2',
+};
+
 export async function startServer() {
-	const fastify = fastifyFactory({ loggerInstance: logger });
+	const fastify = await fastifyFactory({ loggerInstance: logger });
+	const authenticate = { realm: 'Westeros' };
+	fastify.register(basicAuth, { validate, authenticate });
+	async function validate(username, password, req, reply) {
+		if (username !== 'bob' || password !== users.bob) {
+			return new Error('Authorisation failed');
+		}
+	}
+
+	fastify.after(() => {
+		// @ts-ignore
+		fastify.addHook('onRequest', fastify.basicAuth);
+	});
 
 	for (const route of routes) {
 		fastify.route(route);
@@ -20,14 +38,9 @@ export async function startServer() {
 
 	try {
 		await fastify.listen({ port: 6363 });
-	} catch (error: unknown) {
+	} catch (error) {
 		fastify.log.error(error);
-
-		if (error instanceof Error) {
-			throw new TypeError(error.message);
-		}
-
-		throw new Error(String(error));
+		throw error instanceof Error ? new TypeError(error.message) : new Error(String(error));
 	}
 
 	return fastify;
