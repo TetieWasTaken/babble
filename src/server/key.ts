e9e6ea30483223c84c7e6fc791836b6ad2f7119b
TetieWasTaken <tjvssr@gmail.com>
2025-05-28 18:40:50 +0200

    fix: autogenerate keys dir

--- src/server/key.ts
+++ src/server/key.ts
@@ -1,13 +1,22 @@
-import { generateKeyPairSync } from 'node:crypto';
-import { writeFileSync } from 'fs';
+import { generateKeyPairSync } from 'crypto';
+import { mkdirSync, existsSync, writeFileSync } from 'fs';
+import { join } from 'path';
+import logger from '../linker/logger.js';
+
+const keyDir = join(process.cwd(), 'keys');
 
 export function generateKeys() {
+	if (!existsSync(keyDir)) {
+		mkdirSync(keyDir, { recursive: true });
+		logger.info(`Created directory: ${keyDir}`);
+	}
+
 	const { publicKey, privateKey } = generateKeyPairSync('rsa', {
 		modulusLength: 2048,
 		publicKeyEncoding: { type: 'spki', format: 'pem' },
 		privateKeyEncoding: { type: 'pkcs8', format: 'pem' },
 	});
 
-	writeFileSync('./keys/public.pem', publicKey);
-	writeFileSync('./keys/private.pem', privateKey);
+	writeFileSync(join(keyDir, 'public.pem'), publicKey);
+	writeFileSync(join(keyDir, 'private.pem'), privateKey);
 }
