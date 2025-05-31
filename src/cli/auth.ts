132f13deaba3beba42509bba65fcea6843ce375f
TetieWasTaken <tjvssr@gmail.com>
2025-05-31 14:19:00 +0200

    fix: encrypt correctly

--- src/cli/auth.ts
+++ src/cli/auth.ts
@@ -1,26 +1,21 @@
 import fetch from 'node-fetch';
 import { RequestResult } from './index.js';
-
-function pemToArrayBuffer(pem: string) {
-	const b64 = pem.replace(/-----(BEGIN|END) PUBLIC KEY-----/g, '').replace(/\s+/g, '');
-	const raw = Buffer.from(b64, 'base64');
-	return raw.buffer.slice(raw.byteOffset, raw.byteOffset + raw.byteLength);
-}
+import { constants, publicEncrypt } from 'node:crypto';
 
 export async function encryptPassword(password: string): Promise<string> {
-	const response = await fetch('http://localhost:6363/server/pubkey');
-	const json = (await response.json()) as RequestResult;
-	const pubPem = json.data.key;
+	const res = await fetch('http://localhost:6363/server/pubkey');
+	const {
+		data: { key: pubPem },
+	} = (await res.json()) as RequestResult;
 
-	const key = await crypto.subtle.importKey(
-		'spki',
-		pemToArrayBuffer(pubPem),
-		{ name: 'RSA-OAEP', hash: 'SHA-256' },
-		false,
-		['encrypt'],
+	const ciphertext = publicEncrypt(
+		{
+			key: pubPem,
+			padding: constants.RSA_PKCS1_OAEP_PADDING,
+			oaepHash: 'sha256',
+		},
+		Buffer.from(password, 'utf8'),
 	);
 
-	const enc = new TextEncoder();
-	const ciphertext = await crypto.subtle.encrypt({ name: 'RSA-OAEP' }, key, enc.encode(password));
-	return Buffer.from(ciphertext).toString('base64');
+	return ciphertext.toString('base64');
 }
